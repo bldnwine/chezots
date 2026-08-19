60deb4a161ca3101c90c5ab3295cac8d9264cf1a
bldnwine <hwas9048@gmail.com>
2026-08-19 16:58:53 +0530

    kitty layout toggles

--- dot_config/hypr/hyprland.lua
+++ dot_config/hypr/hyprland.lua
@@ -381,8 +381,8 @@ for i = 1, 10 do
 	hl.bind(mainMod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = i }))
 end
 
-hl.bind(mainMod .. " + I", hl.dsp.workspace.toggle_special("scratchpad"))
-hl.bind(mainMod .. " + SHIFT + I", hl.dsp.window.move({ workspace = "special:scratchpad" }))
+hl.bind(mainMod .. " + D", hl.dsp.workspace.toggle_special("scratchpad"))
+hl.bind(mainMod .. " + SHIFT + D", hl.dsp.window.move({ workspace = "special:scratchpad" }))
 hl.bind(mainMod .. " + S", hl.dsp.workspace.toggle_special("magic"))
 hl.bind(mainMod .. " + SHIFT + S", hl.dsp.window.move({ workspace = "special:magic" }))
 
